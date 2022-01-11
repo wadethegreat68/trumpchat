82369cced313b2663c03e152f1e8c6030c3411a2
wadethegreat68 <45704617+wadethegreat68@users.noreply.github.com>
2022-01-11 09:09:32 -0700

    Delete compose.js

--- compose.js
+++
@@ -1,5 +0,0 @@
-export default function compose(...fns) {
-  const firstFn = fns[fns.length - 1]
-  const remainingFns = fns.slice(0, -1)
-  return x => remainingFns.reduceRight((acc, fn) => fn(acc), firstFn(x))
-}
